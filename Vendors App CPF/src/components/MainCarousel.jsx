b07852dc4d6a4d95423722e0156b518a8bb6f32c
Alfredo <offsound2020@gmail.com>
2024-03-04 16:29:58 -0400

    Cambios Alfredo

--- Vendors App CPF/src/components/MainCarousel.jsx
+++ Vendors App CPF/src/components/MainCarousel.jsx
@@ -10,32 +10,59 @@ import "swiper/css/effect-cards";
 import "../scss/MainCarousel.scss";
 
 // import required modules
-import { EffectCards } from "swiper/modules";
+import { EffectCoverflow } from "swiper/modules";
 
 export default function App() {
   return (
     <>
-      <motion.div className="main-carousel-container"
-        initial={{y: 500, opcacity: 0}}
-        whileInView={{y: 0, opcacity: 1}}
-        transition={{duration: 0.5}}
-      
-        >
+      <motion.div
+        className="main-carousel-container"
+        initial={{ y: 500, opcacity: 0 }}
+        whileInView={{ y: 0, opcacity: 1 }}
+        transition={{ duration: 0.5 }}
+      >
         <Swiper
-          effect={"cards"}
+          effect={"coverflow"}
           grabCursor={false}
-          modules={['coverflow']}
+          modules={[EffectCoverflow]}
           loop={false}
+          slidesPerView={3}
+          coverflowEffect={{
+            rotate: 50,
+            stretch: 0,
+            depth: 100,
+            modifier: 1,
+            slideShadows: false,
+          }}
+          breakpoints={{
+            940: {
+              slidesPerView: 3,
+              slidesPerGroup: 1,
+            },
+            340: {
+              slidesPerView: 1,
+              slidesPerGroup: 1,
+            }
+          }}
           className="mySwiper"
         >
           <SwiperSlide>
-            <Link to="/photo-bank">Photo Bank</Link>
+            <Link to="/photo-bank">
+              <img src="/src/assets/Mockup1.jpg" alt="" />
+              <span>Photo Bank</span>
+            </Link>
           </SwiperSlide>
           <SwiperSlide>
-            <Link to="/technnical-data">Technnical Data</Link>
+            <Link to="/technnical-data">
+              <img src="/src/assets/Mockup.jpg" alt="" />
+              <span>Technnical Data</span>
+            </Link>
           </SwiperSlide>
           <SwiperSlide>
-            <Link to="/merch">Merch</Link>
+            <Link to="/merch">
+              <img src="/src/assets/D54 - Light Oak 1.jpg" alt="" />
+              <span>Merch</span>
+            </Link>
           </SwiperSlide>
         </Swiper>
       </motion.div>
