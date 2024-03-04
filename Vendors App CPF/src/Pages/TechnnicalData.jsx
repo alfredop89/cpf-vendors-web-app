f37d218f0fc306110b64b37c73e1474f170ba742
Alfredo <offsound2020@gmail.com>
2024-03-04 16:44:51 -0400

    Final commit for today

--- Vendors App CPF/src/Pages/TechnnicalData.jsx
+++ Vendors App CPF/src/Pages/TechnnicalData.jsx
@@ -2,14 +2,21 @@ import TechSidebar from "../Sidebars/TechSidebar";
 import Header from "../components/Header";
 import { lazy, Suspense } from "react";
 import Loader from "../components/Loader";
+import "../scss/TechnnicalData.scss"
 
 function TechnnicalData() {
   return (
     <>
       <Suspense fallback={<Loader />}>
       <Header />
+
+      <div className="technnical-data-container">
+          <h1>CPF Technnical Data</h1>
+        </div>
       <TechSidebar />
       </Suspense>
+
+
     </>
   );
 }
