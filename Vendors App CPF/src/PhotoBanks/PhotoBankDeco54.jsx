da5156e03104faf8b45da079a8795b498056424c
Alfredo <offsound2020@gmail.com>
2024-03-05 15:31:53 -0400

    Tercer cambio 05/03/2024

--- Vendors App CPF/src/PhotoBanks/PhotoBankDeco54.jsx
+++ Vendors App CPF/src/PhotoBanks/PhotoBankDeco54.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import Header from "../components/Header";
 import PhotoBankSidebar from "../Sidebars/PhotoBankSidebar";
 import Deco54Carousel from "../RoomScenesCarousels/Deco54Carousel";
+import Deco54CarouselInstallation from "../RoomScenesCarousels/Deco54CarouselInstallation"
+import Footer from "../components/Footer"
 
 function PhotoBankDeco54() {
   return (
@@ -11,7 +13,10 @@ function PhotoBankDeco54() {
       <div className="photobank-container">
         <h2>Room Scenes - Deco54</h2>
         <Deco54Carousel />
+        <h2>Installation</h2>
+        <Deco54CarouselInstallation />
       </div>
+      <Footer />
     </>
   );
 }
